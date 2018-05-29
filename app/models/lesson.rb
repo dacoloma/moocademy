827f8f30937b29e081358a15015c1a720257e386
D.Coloma <dnl.coloma@gmail.coma>
2018-05-29 15:27:29 +0200

    [FIX] Vulnerabilities from dependancies

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,5 +1,5 @@
 class Lesson < ApplicationRecord
-  #belongs_to :course
+  belongs_to :course
   validates :title, presence: true, length: {maximum: 100}
   validates :body, presence: true
   validates :course_id, presence: true
